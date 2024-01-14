19cde50b4d8a8b36158f16b1977e8ca325dcf27e
Egert Tappo <egert.tappo@tktk.ee>
2024-01-14 03:28:32 +0200

    add code w4

---
+++ FOR-loopSAM/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int sum = 0;
+
+for (int i = 10; i > 0; i--)
+{ Console.WriteLine("i = " +i+"!");
+  Console.WriteLine($"i = {i}!");
+  Console.WriteLine(sum);
+    //sum = sum +1;
+   sum += i;
+};
+
+
+Random rand = new Random(); 
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 0;
+
+for (i =0; i <3  ; i++)
+{
+    Console.WriteLine("Hello, World!"+ (i)  +    "aastane" + "xd" +  "     " +   "xd") ;
+    Console.WriteLine(i);
+}
+
+Console.WriteLine(" World!");
+
+
